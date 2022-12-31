4cef9827ae96ee8d9c351f9bce04b71c13b1b538
Tatiana-Filimonova <virtroll@yandex.ru>
2022-12-31 08:35:39 +0300

    deleted unnecessary files and renamed ner_model.py

--- NER_model.py
+++ NER_model.py
@@ -1,6 +1,12 @@
 from transformers import pipeline
 
-classifier = pipeline("ner",   
-                      "jplu/tf-xlm-r-ner-40-lang")
+nlp_ner = pipeline(
+    "ner",
+    model="jplu/tf-xlm-r-ner-40-lang",
+    tokenizer=(
+        'jplu/tf-xlm-r-ner-40-lang',  
+        {"use_fast": False}),
+    framework="tf"
+)
 
-classifier("서귀포시 강창식 전 국장 등 25명 정년·명예퇴임")
+nlp_ner("서귀포시 강창식 전 국장 등 25명 정년·명예퇴임")
--- from_card.py
+++
@@ -1,2 +0,0 @@
-f = open("C:\\Users\\Татьяна/.cache\\huggingface\\hub\\models--jplu--tf-xlm-r-ner-40-lang\\snapshots\\4400c6fa835523b5f141ab4eaa543c5b89ddc322\\sentencepiece.bpe.model", "r")
-print(f.readline())
