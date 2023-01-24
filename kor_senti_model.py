f9ef3d9d706b2632d9274b11ac15df9a1147705a
Tatiana-Filimonova <virtroll@yandex.ru>
2023-01-24 07:35:55 +0300

    edited

--- kor_senti_model.py
+++ kor_senti_model.py
@@ -1,7 +1,6 @@
 from transformers import pipeline
-import streamlit as st
 
 classifier = pipeline("sentiment-analysis",   
                       "snunlp/KR-FinBert-SC")
 
-st.write(classifier(st.text_input('Введите фразу для оценки...')))
+print(classifier("C쇼크에 멈춘 흑자비행…대한항공 1분기 영업적자 566억"))
