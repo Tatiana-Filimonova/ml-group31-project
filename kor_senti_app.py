29cf12ea2ef16cf272e726952c45d9483b513744
Tatiana-Filimonova <virtroll@yandex.ru>
2023-01-27 23:27:47 +0300

    changed the app

--- kor_senti_app.py
+++ kor_senti_app.py
@@ -2,24 +2,22 @@ import io
 import streamlit as st
 import numpy as np
 from transformers import pipeline
-#from tensorflow.keras.applications import EfficientNetB0
-#from tensorflow.keras.saving.hdf5_format import 
-#from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 classifier = pipeline("sentiment-analysis",
                       "snunlp/KR-FinBert-SC")
 
+st.header('Оценка эмоциональной окраски текстов на корейском языке')
 form = st.form(key='my_form')
-text = form.text_input(label='Enter some text')
-submit_button = form.form_submit_button(label='Submit')
+text = form.text_input(label='Введите текст')
+submit_button = form.form_submit_button(label='Выполнить')
 
 def create_score_text(clsfr, txt):
     result = clsfr(txt)
-    st.write('Оценка фразы: ' + result[0].get('label', 'no res'))
+    st.write('**Оценка фразы:** ' + result[0].get('label', 'no res'))
     if result[0]['score'] >= 0.75:
-        return 'Верим (score >= 0.75)'
+        return '**Степень достоверности:** достоверный результат (score >= 0.75)'
     else:
-        return 'Но это не точно (score < 0.75)'
+        return '**Степень достоверности:** точность невысокая (score < 0.75)'
 
 if submit_button:
     res_func = create_score_text(classifier,text)
